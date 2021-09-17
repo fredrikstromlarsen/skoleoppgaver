0c57abbe95d7bbbe3440befab335cdb520d568f4
fredrikstromlarsen <07fredrik@gmail.com>
2021-09-17 11:29:59 +0200

    Figuring out how to serve static assets with ExpressJS

--- node-express-db/index.js
+++ node-express-db/index.js
@@ -1,3 +1,6 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 80;
@@ -15,12 +18,31 @@ con.connect(function (err) {
 	console.log('Connected to MySQL DB');
 	con.query(query, function (err, result) {
 		if (err) throw err;
-		app.get('/', (req, res) => {
-			res.send(result);
-		});
+		console.log('MySQL query ran');
 	});
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port, () =>
+	console.info(`App listening on port ${port}`)
+	
+	app.use(express.static(`public`))
+);
+/* 
+http.createServer(function (req, res) {
+	var q = url.parse(req.url, true);
+	var filename = '.' + q.pathname;
+	console.log(`filename = ${filename}`);
+
+	fs.readFile(filename, function (err, data) {
+		if (err && q != '/') {
+			res.writeHead(404, { 'Content-Type': 'text/html' });
+			return res.end('This file/folder does not exist');
+		}
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write(
+			data
+		);
+		res.end();
+	});
+}).listen(80);
+ */
