4ae3c006d347fb1e70037e947c561e4ad3262cd4
fredrikstromlarsen <07fredrik@gmail.com>
2021-09-30 15:43:13 +0200

    Added new section: How to change font-size with CSS

--- template-website/js/main.js
+++ template-website/js/main.js
@@ -1,4 +1,6 @@
-var inputArray = document.querySelectorAll('input');
+var inputArray = document.querySelectorAll('input, .input')
+console.log(inputArray);
+
 for (i = 0; i < inputArray.length; i++) {
 	inputArray[i].addEventListener('input', checkLang(this));
 }
@@ -18,9 +20,7 @@ function tryHTML(element) {
 			// Legger til en alt="" attribute på slutten av
 			// img - taggen, denne teksten vises når bildet
 			// ikke kan lastes inn.
-			output =
-				code.substring(0, code.length - 1) +
-				' alt="Dette funket ikke, sjekk at URLen lenker direkte til et bilde.">';
+			output = code.substring(0, code.length - 1) + ' alt="Dette funket ikke, sjekk at URLen lenker direkte til et bilde.">';
                 errorElement.style.opacity = '0%';
                 errorElement.style.maxHeight = '0rem';
 		} else if (code == '') {
@@ -34,6 +34,15 @@ function tryHTML(element) {
 		}
 		outputElement.innerHTML = output;
 	}
+}
+
+function tryCSS(element) {
+    var stylesheet = window.document.styleSheets[0];
+    var code = element.innerHTML;
+    if (code.match(/^(h\d{1,}\s+{\s+font-size:\s+\d+(px|rem|em|ch|cm|mm|in|%|pt|pc|ex|vw|vh|vmin|vmax);\s+})$/g)) {
+        output = ".col-right " + code;
+        stylesheet.insertRule(output, stylesheet.cssRules.length);
+    }
 }
 
 function checkLang(element) {
