9cc422a5c08aa23b4251432c4123b1f81360d31e
fredrikstromlarsen <07fredrik@gmail.com>
2022-02-06 14:39:13 +0100

    Math

--- python/2022-02-06/main.py
+++ python/2022-02-06/main.py
@@ -3,7 +3,7 @@ r = 0
 a = float(input("Number to calculate the root from: \n"))
 b = float(input("Exponent to use: \n"))
 c = float("1" + "0" * math.floor(math.log10(math.sqrt(a))))
-while (r + 0.00001) ** b < a:
+while (r + 0.0000000000001) ** b < a:
     if (r + c) ** b < a:
         r += c
     else:
