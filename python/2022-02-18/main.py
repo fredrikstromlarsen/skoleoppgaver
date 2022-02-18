ca87f9254a2e77f4112acce98b4278f00732ad69
fredrikstromlarsen <07fredrik@gmail.com>
2022-02-18 13:27:08 +0100

    Done

--- python/2022-02-18/main.py
+++ python/2022-02-18/main.py
@@ -13,56 +13,56 @@ def a(arr):
     return sum
 
 
-print(f"a) Summen av alle tall i listen er: {a(randomNumbers)}")
+print(f"a) Summen av alle tall i listen er: {a(randomNumbers)}\n")
 
 
 def b(arr):
     sorted_arr = sorted(arr)
     arr_len = len(arr)
-    if arr_len % 2 == 0:
-        first = sorted_arr[math.floor((arr_len - 1) / 2)]
-        second = sorted_arr[math.ceil((arr_len - 1) / 2)]
-        median = (first + second) / 2
-    else:
-        median = sorted_arr[math.floor(arr_len / 2)]
+    first = sorted_arr[math.floor((arr_len - 1) / 2)]
+    second = sorted_arr[math.ceil((arr_len - 1) / 2)]
+    median = (first + second) / 2
     return median
 
 
-print(f"b) Medianen i listen er: {b(randomNumbers)}")
+print(f"b) Medianen i listen er: {b(randomNumbers)}\n")
 
 
 def c(arr):
     return a(arr) / len(arr)
 
 
-print(f"c) Gjennomsnittet i listen er: {c(randomNumbers)}")
+print(f"c) Gjennomsnittet i listen er: {c(randomNumbers)}\n")
 
 
 def d(arr, i):
     return arr[i]
 
-print(f"d) Index 4 i listen er: {d(randomNumbers, 3)}, Index 18 i listen er: {d(randomNumbers, 17)}")
+
+print(f"d) Index 4 i listen er: {d(randomNumbers, 3)}, Index 18 i listen er: {d(randomNumbers, 17)}\n")
 
 
 def e(arr):
     return max(arr)
 
 
-print(f"e) Det høyeste tallet i listen er: {e(randomNumbers)}")
+print(f"e) Det høyeste tallet i listen er: {e(randomNumbers)}\n")
 
 
 def f(arr):
     return list(dict.fromkeys(arr))
 
+
 print(f"f) Listen uten duplikater:")
 pprint(f(randomNumbers))
+print("")
 
 
 def g(arr):
     return arr[-5::]
 
 
-print(f"g) De siste 5 elementene i listen er: {g(randomNumbers)}")
+print(f"g) De siste 5 elementene i listen er: {g(randomNumbers)}\n")
 
 
 def h(arr):
@@ -70,7 +70,7 @@ def h(arr):
     return a(half)
 
 
-print(f"h) Summen av annenhvert tall i listen er: {h(randomNumbers)}")
+print(f"h) Summen av annenhvert tall i listen er: {h(randomNumbers)}\n")
 
 
 def i(arr, indexes, values):
@@ -81,4 +81,28 @@ def i(arr, indexes, values):
 
 print(f"i) Gammel liste øverst og ny liste nederst.")
 pprint(randomNumbers)
-pprint(i(randomNumbers,))
+pprint(i(randomNumbers,[3, 6, 15], [1, 2, 3]))
+print("")
+
+
+def j(arr):
+    arr.append(random.randint(0, 50))
+    return arr
+
+
+print(f"j) Et nytt tall er lagt til i slutten av listen:")
+pprint(j(randomNumbers))
+print("")
+
+
+def k(arr, multiplicand, max_val):
+    products = []
+    multiplier = 0
+    for multiplier in range(math.floor(max_val/multiplicand)):
+        products.append(multiplicand * multiplier)
+
+    return products
+
+print("k) 3-gangeren er som følger:")
+pprint(k(randomNumbers, 3, 100))
+print("")
