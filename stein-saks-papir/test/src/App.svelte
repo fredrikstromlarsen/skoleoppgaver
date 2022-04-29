24976a296f19178bfd19349ffca81a5fa8f4c988
fredrikstromlarsen <07fredrik@gmail.com>
2022-04-29 11:27:16 +0200

    Mekanikken i spillet er enkel

---
+++ stein-saks-papir/test/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	// Stein-, saks-, papirmekanikk
+	let result = 0
+	let test
+	let machineAction
+	const player = ["player", "machine", "tie"];
+
+	export function game(playerAction){
+		machineAction = Math.floor(Math.random() * 3);
+
+		if (playerAction === machineAction) result = 2;
+		else if ((playerAction + 1) % 3 === machineAction) result = 0;
+		else result = 1;
+
+		test = player[result]
+	}
+
+	
+
+</script>
+
+<main>
+	<h1>{test}</h1>
+	<button on:click={() => game(0)}>Stein</button>
+	<button on:click={() => game(1)}>Saks</button>
+	<button on:click={() => game(2)}>Papir</button>
+</main>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
