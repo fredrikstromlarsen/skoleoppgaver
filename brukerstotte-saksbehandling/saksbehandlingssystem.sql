3d7dfd52eb7b07e5f6375dff9b36c9e04bb5f878
fredrikstromlarsen <07fredrik@gmail.com>
2022-05-17 23:16:00 +0200

    Denne er viktig :p

--- brukerstotte-saksbehandling/saksbehandlingssystem.sql
+++ brukerstotte-saksbehandling/saksbehandlingssystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 15, 2022 at 05:20 PM
+-- Generation Time: May 17, 2022 at 11:15 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -24,38 +24,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cases`
+-- Table structure for table `tickets`
 --
 
-CREATE TABLE `cases` (
+CREATE TABLE `tickets` (
   `id` int(11) NOT NULL,
   `type` varchar(3) NOT NULL,
   `impact` int(1) NOT NULL,
   `urgency` int(1) NOT NULL,
   `description` text NOT NULL,
-  `category` varchar(16) NOT NULL,
+  `category` text NOT NULL,
   `page` text NOT NULL,
   `status` int(1) NOT NULL DEFAULT 0,
   `registered` datetime NOT NULL,
   `started` datetime DEFAULT NULL,
-  `finished` datetime DEFAULT NULL
+  `finished` datetime DEFAULT NULL,
+  `email` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `cases`
+-- Dumping data for table `tickets`
 --
 
-INSERT INTO `cases` (`id`, `type`, `impact`, `urgency`, `description`, `category`, `page`, `status`, `registered`, `started`, `finished`) VALUES
-(5, 'CHG', 1, 2, 'If there are more than zero rows returned, the function fetch_assoc() puts all the results into an associative array that we can loop through. The while() loop loops through the result set and outputs the data from the id, firstname and lastname columns.\r\n\r\nThe following example shows the same as the example above, in the MySQLi procedural way:', 'Rettigheter', 'https://www.w3schools.com/php/php_mysql_select.asp', 2, '2022-05-15 17:11:04', '2022-05-15 17:17:03', '2022-05-15 17:17:23');
+INSERT INTO `tickets` (`id`, `type`, `impact`, `urgency`, `description`, `category`, `page`, `status`, `registered`, `started`, `finished`, `email`) VALUES
+(21, 'INC', 3, 2, '', '', '', 2, '2022-05-17 22:41:17', '2022-05-17 22:52:48', '2022-05-17 23:10:08', ''),
+(22, 'INC', 3, 2, '', '', '', 2, '2022-05-17 22:47:37', '2022-05-17 22:53:00', '2022-05-17 23:10:09', ''),
+(23, 'INC', 3, 2, '', '', '', 1, '2022-05-17 22:54:41', '2022-05-17 23:09:57', NULL, ''),
+(24, 'INC', 2, 1, '', '', '', 2, '2022-05-17 22:55:49', '2022-05-17 23:09:58', '2022-05-17 23:11:54', ''),
+(25, 'INC', 3, 1, '', '', '', 2, '2022-05-17 23:02:57', '2022-05-17 23:12:03', '2022-05-17 23:13:01', '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `cases`
+-- Indexes for table `tickets`
 --
-ALTER TABLE `cases`
+ALTER TABLE `tickets`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -63,10 +68,10 @@ ALTER TABLE `cases`
 --
 
 --
--- AUTO_INCREMENT for table `cases`
+-- AUTO_INCREMENT for table `tickets`
 --
-ALTER TABLE `cases`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `tickets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
